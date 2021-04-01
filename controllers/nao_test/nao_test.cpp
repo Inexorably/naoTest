1ef1f7f9ce2df5e61eb78aa23f3a485d01bd2c24
Inexorably <ssantillo@ucla.edu>
2021-04-02 00:39:14 +0900

    Simple testing / io

--- controllers/nao_test/nao_test.cpp
+++ controllers/nao_test/nao_test.cpp
@@ -11,6 +11,9 @@
 #include <webots/Motor.hpp>
 #include <webots/camera.hpp>
 
+// For supervisor / COM etc
+#include <webots/Node.hpp>
+
 #include <webots/accelerometer.hpp>
 #include <webots/camera.hpp>
 #include <webots/DistanceSensor.hpp>
@@ -23,6 +26,7 @@
 #include <webots/TouchSensor.hpp>
 #include <webots/utils/motion.hpp>
 
+#include <iostream>
 #include <math.h>
 
 // All the webots classes are defined in the "webots" namespace
@@ -53,18 +57,24 @@ int main(int argc, char **argv) {
   
   Motor *headYaw = robot->getMotor("HeadYaw");
   Motor *RElbowRoll = robot->getMotor("RElbowRoll");
-  Motor *RHipPitch = robot->getMotor("RHipPitch")
-  Motor *LShoulderPitch = robot->getMotor("LShoulderPitch");;
+  Motor *RHipPitch = robot->getMotor("RHipPitch");
+  Motor *LShoulderPitch = robot->getMotor("LShoulderPitch");
+  Motor *RShoulderPitch = robot->getMotor("RShoulderPitch");
   
   Camera *cameraTop = robot->getCamera("CameraTop");
   Camera *cameraBottom = robot->getCamera("CameraBottom");
   cameraTop->enable(4 * timeStep);
   cameraBottom->enable(4 * timeStep);
+  
+  // Set arms down.
+  LShoulderPitch->setPosition(0);
+  RShoulderPitch->setPosition(M_PI/2);
 
 
   // Main loop:
   // - perform simulation steps until Webots is stopping the controller
   while (robot->step(timeStep) != -1) {
+    double t = robot->getTime();
     // Read the sensors:
     // Enter here functions to read sensor data, like:
     //  double val = ds->getValue();
@@ -74,14 +84,28 @@ int main(int argc, char **argv) {
     // Enter here functions to send actuator commands, like:
     //  motor->setPosition(10.0);
     
+    // Print some info:
+    static int ticker = 0;
+    ticker++;
+    if (ticker > 30) {
+      std::cout << "t = " << t << ", COM = " << robot->getCenterOfMass() << std::endl; 
+      ticker = 0;
+    }
     
     // Do some linear interpolation with sin centered around sin(t-pi/2).  Then:
     // max at t%2pi == pi, min at t%2pi == 0.
     // interpolation: y = y1 + ((x – x1) / (x2 – x1)) * (y2 – y1)
-    double interpolated = RHipPitch->getMinPosition() + ((sin(robot->getTime()-M_PI/2) - -1) / (1 - -1) * (0 - RHipPitch->getMinPosition()));
-    RHipPitch->setPosition(interpolated);
-    interpolated = LShoulderPitch->getMinPosition() + ((sin(robot->getTime()-M_PI/2) - -1) / (1 - -1) * (0 - LShoulderPitch->getMinPosition()));
-    LShoulderPitch->setPosition(interpolated);
+    double interpolated = 0.7*(RHipPitch->getMinPosition() + ((sin(robot->getTime()/3.0) - -1) / (1 - -1) * (0 - RHipPitch->getMinPosition())));
+    //RHipPitch->setPosition(interpolated);
+/*
+    interpolated = LShoulderPitch->getMinPosition() + ((sin(robot->getTime()/3.0-M_PI)+1 - 0) / (2 - 0) * (LShoulderPitch->getMaxPosition() - 0));
+    LShoulderPitch->setPosition(LShoulderPitch->getMaxPosition());
+    
+
+    interpolated = RShoulderPitch->getMinPosition() + ((sin(robot->getTime()) - -1) / (1 - -1) * (0 - RShoulderPitch->getMinPosition()));
+    RShoulderPitch->setPosition(1.5);
+  */  
+
   };
 
   // Enter here exit cleanup code.
