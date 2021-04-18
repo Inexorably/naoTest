b73a97067cb63ce2b3944e95526ae17c1268af9c
Inexorably <ssantillo@ucla.edu>
2021-04-18 23:02:34 +0900

    Add getStdDev() to utilities

--- controllers/nao_test/utilities.cpp
+++ controllers/nao_test/utilities.cpp
@@ -165,3 +165,18 @@ bool isStable(TouchSensor *fsrL, TouchSensor *fsrR){
   
   return newtonLeft + newtonRight > FOOT_FORCE_MIN;
 }
+
+// Takes a vector of doubles v.
+// Returns the population standard deviation NOT sample standard deviation.
+// Source since lazy: https://stackoverflow.com/questions/7616511/
+double getStdDev(const std::vector<double>& v) {
+  double sum = std::accumulate(v.begin(), v.end(), 0.0);
+  double mean = sum / v.size();
+
+  std::vector<double> diff(v.size());
+  std::transform(v.begin(), v.end(), diff.begin(), [mean](double x) { return x - mean; });
+  double sq_sum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+  double stdev = std::sqrt(sq_sum / v.size());
+
+  return stdev;
+}
